802b1ebddc1390d66be16adcb2806791f1791ed6
UMAsouls <umaplay972@gmail.com>
2024-09-01 22:29:26 +0900

    [add] add IPorn and BattlePorn

--- Assets/PhysicalBurstProject/Interface/IPorn.cs
+++ Assets/PhysicalBurstProject/Interface/IPorn.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IPorn : MonoBehaviour
+public interface IPorn
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float attack {  get; }
+    public float defence { get; }
+    public float speed { get; }
+    public float range { get; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool death { get; }
 }
---
+++ Assets/PhysicalBurstProject/Script/Porn/BattlePorn.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlePorn : MonoBehaviour, IPorn
+{
+
+
+    public float attack 
+    {
+        get { return 0; }
+    }
+
+    public float defence
+    {
+        get { return 0; }
+    }
+
+    public float speed
+    {
+        get { return 0; }
+    }
+
+    public float range
+    {
+        get { return 0; }
+    }
+
+    public bool death
+    {
+        get { return false; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
