b46c28eac531bc5c8c7334fa2312a84633150029
UMAsouls <umaplay972@gmail.com>
2024-10-17 18:10:25 +0900

    [add] camera managerの設計図

---
+++ Assets/PhysicalBurstProject/Script/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.PhysicalBurstProject.Script
+{
+    public class CameraManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
