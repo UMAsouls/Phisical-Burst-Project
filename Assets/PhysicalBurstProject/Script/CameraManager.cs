7af862d63738deae0cd820088de5cc88031211fd
UMAsouls <umaplay972@gmail.com>
2024-10-17 18:10:43 +0900

    ちょっと修正

--- Assets/PhysicalBurstProject/Script/CameraManager.cs
+++ Assets/PhysicalBurstProject/Script/CameraManager.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace Assets.PhysicalBurstProject.Script
+public class CameraManager : MonoBehaviour
 {
-    public class CameraManager : MonoBehaviour
+
+    // Use this for initialization
+    void Start()
     {
 
-        // Use this for initialization
-        void Start()
-        {
+    }
 
-        }
+    // Update is called once per frame
+    void Update()
+    {
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
