b3d41fe8bcba1acacdec489795faac01919a075d
UMAsouls <umaplay972@gmail.com>
2024-10-23 12:31:48 +0900

    区切り

---
+++ Assets/PhysicalBurstProject/Script/UI/SlotFrameAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotFrameAnimator : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool Burst;
+
+
+    private Material mat;
+
+    private void SetBurst(bool v)
+    {
+        if (v) mat.SetInt("_Burst", 1);
+        else mat.SetInt("_Burst", 0);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        mat = GetComponent<Image>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
