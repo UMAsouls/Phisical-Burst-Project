39db2ca8ac011f3ac21f3b9cacf746bd847cd0bf
UMAsouls <umaplay972@gmail.com>
2025-10-23 00:10:44 +0900

    [add] PubSubパターン用インターフェース

---
+++ Assets/PhysicalBurstProject/Interface/common/IBroker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IBroker<V,T>
+{
+    void Subscribe(V topic, ISubscriber<T> subscriber);
+    void BroadCast(V topic, T message);
+}
